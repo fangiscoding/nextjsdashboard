da4b51e90a2b41a977d55744317c50db5aff65be
fangiscoding <fchenroyal@gmail.com>
2025-07-11 07:04:25 -0400

    Added search

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false, // This is the options that hide/remove the icon
+  // experimental: {
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
